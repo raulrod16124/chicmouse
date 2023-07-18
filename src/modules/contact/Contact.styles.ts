42e73e8869ace7438e94616dad4e48f742602302
raulrod <raul.rod16124@gmail.com>
2023-07-18 22:20:58 +0100

    update the contact page UI

--- src/modules/contact/Contact.styles.ts
+++ src/modules/contact/Contact.styles.ts
@@ -6,27 +6,68 @@ export const ContactWrapper= styled.div`
     height: auto;
     display: flex;
     justify-content: center;
-    margin-top: 80px;
     margin-bottom: 100px;
+`;
+
+export const ContactTitle= styled.p`
+    position: absolute;
+    top: 120px;
+    width: 100%;
+    font-size: 25px;
+    font-weight: 600;
+    font-style: italic;
+    text-align: center;
+    
     @media ${device.laptop} { 
-        margin-top: 50px;
+        font-size: 20px;
+        width: 90%;
+    }
+    
+    @media ${device.mobileL} { 
+        font-size: 15px;
+        width: 90%;
     }
 `;
 
 export const ContactContent= styled.div`
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding: 20px;
-    width: 400px;
+    justify-content: space-around;
+    align-items: center;
+    padding: 0 20px;
+    width: 1000px;
+    @media ${device.laptop} { 
+       flex-direction: column;
+    }
+`;
+
+export const ContactImage = styled.img`
+    align-self: center;
+    width: 350px;
+    height: 500px;
+    border-radius: 20px;
+    margin-top: 50px;
+    @media ${device.laptop} { 
+        margin-top: 80px;
+        width: 180px;
+        height: 250px;
+    }
+    @media ${device.mobileL} { 
+        margin-top: 80px;
+    }
 `;
 
 export const CustomForm = styled.form`
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 100%;
+    width: 400px;
     height: 420px;
+    margin-top: 120px;
+    @media ${device.laptop} { 
+        margin-top: 20px;
+        width: 90%;
+        margin-bottom: 200px;
+    }
 `;
 
 export const SendingContent = styled.form`

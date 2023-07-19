0dcde57132ebff58e8f445b7eebafe517c78f594
raulrod <raul.rod16124@gmail.com>
2023-07-19 14:46:41 +0100

    include favicon in the html and the error page ui

--- src/common/ErrorPage.tsx
+++ src/common/ErrorPage.tsx
@@ -1,5 +1,24 @@
+import { 
+  ErrorPageContent, 
+  ErrorPageImg, 
+  ErrorPageText, 
+  ErrorPageWrapper, 
+  RedirectButton
+} from "./ErrorPage.styles"
+import mouseConfusedImage from "assets/mouseConfusedImage.png";
+
 export const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <ErrorPageWrapper>
+      <ErrorPageContent>
+        <ErrorPageImg src={mouseConfusedImage} alt="mouse-confused-image" />
+        <ErrorPageText>
+          Ups, no se ha encontrado la página que buscaba.
+        </ErrorPageText>
+        <RedirectButton to={"/"}>
+          Ir al inicio
+        </RedirectButton>
+      </ErrorPageContent>
+    </ErrorPageWrapper>
   )
 }
--- src/common/Logo.styles.ts
+++ src/common/Logo.styles.ts
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { device } from "utils";
 
-export const LogoWrapper= styled.div`
+export const LogoWrapper = styled.div`
     width: 100%;
     height: 100%;
 `;
 
-export const LogoLink= styled(Link)`
+export const LogoLink = styled(Link)`
     margin: 0 auto;
 `;
 
-export const LogoImg= styled.img`
+export const LogoImg = styled.img`
     margin-top: 15px;
     width: 65px;
     height: 55px;

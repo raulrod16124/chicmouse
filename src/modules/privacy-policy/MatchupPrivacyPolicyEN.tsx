5b38976d05c4655f89ff5929ab2f594cbf98f9e8
raulrod <raul.rod16124@gmail.com>
2024-01-03 21:00:56 +0000

    remove text from privacy policy

--- src/modules/privacy-policy/MatchupPrivacyPolicyEN.tsx
+++ src/modules/privacy-policy/MatchupPrivacyPolicyEN.tsx
@@ -25,15 +25,7 @@ export const MatchupPrivacyPolicyEN = () => {
         <Text>
           We use Your Personal data to provide and improve the Service. By using
           the Service, You agree to the collection and use of information in
-          accordance with this Privacy Policy. This Privacy Policy has been
-          created with the help of the{" "}
-          <a
-            href="https://www.privacypolicies.com/privacy-policy-generator/"
-            target="_blank"
-          >
-            Privacy Policy Generator
-          </a>
-          .
+          accordance with this Privacy Policy.
         </Text>
         <TextSubtitle>Interpretation and Definitions</TextSubtitle>
         <TextSecondarySubtitle>Interpretation</TextSecondarySubtitle>
